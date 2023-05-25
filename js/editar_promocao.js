2f5cf48f30e9f30df5f847081d0fc1f001344901
paulo.goncalves <paulo.henriqueicm2011@gmail.com>
2023-05-24 22:01:45 -0300

    Criand a função pesquisarPromocao

--- js/editar_promocao.js
+++ js/editar_promocao.js
@@ -1,13 +1,15 @@
 const inpuntcodpromocao = document.getElementById("codpromo");
 
-inpuntcodpromocao.addEventListener("change", () => {
+const pesquisarPromocao = () => {
+
+  console.log("Entrou na função");
+
   let codpromo = document.getElementById("codpromo").value;
   let descricao = document.getElementById("descricao");
   let dtaini = document.getElementById("dtaini");
   let dtafin = document.getElementById("dtafin");
   let status = document.getElementById("status");
-  //descricao.value = "TESTE";
-  console.log(descricao);
+
   $.getJSON("pesquisar_promocao.php", {
     codpromo,
   }).done(function (retorno) {
@@ -35,40 +37,10 @@ inpuntcodpromocao.addEventListener("change", () => {
       },
     });
   });
+
+}
+
+inpuntcodpromocao.addEventListener("change", () => {
+  pesquisarPromocao(); 
 });
 
-// $(".codpromo").change(function () {
-//   var codpromo = $(".codpromo").val();
-//   var descricao = $(".descricao");
-//   var dtaini = $(".dtaini");
-//   var dtafin = $(".dtafin");
-//   var status = $(".status");
-
-//   $.getJSON("pesquisar_promocao.php", {
-//     codpromo,
-//   }).done(function (retorno) {
-//     descricao.val(retorno.DESCRICAO);
-//     dtaini.val(retorno.DTAINICIO);
-//     dtafin.val(retorno.DTAFIM);
-//     status.val(retorno.STATUS);
-//     var status1 = $(".status").val();
-
-//     if (status1 == "I") {
-//       $(".ultimo").show();
-//       $(".primeiro").hide();
-//     }
-//     if (status1 == "A") {
-//       $(".ultimo").hide();
-//       $(".primeiro").show();
-//     }
-
-//     $.ajax({
-//       url: "editar_produto_emp.php",
-//       method: "get",
-//       data: "codpromo=" + codpromo,
-//       success: function (editar_produto_emp) {
-//         $(".section").empty().html(editar_produto_emp);
-//       },
-//     });
-//   });
-// });
